958af5e2506e53258d9b59b99f2f1341ae14d31a
Aaron Patterson <tenderlove@ruby-lang.org>
2022-01-12 19:25:11 -0800

    bump version

--- lib/myhidapi.rb
+++ lib/myhidapi.rb
@@ -1,7 +1,7 @@
 require "myhidapi.so"
 
 module MyHIDAPI
-  VERSION = '1.0.1'
+  VERSION = '1.0.2'
 
   class DeviceInfo
     attr_reader :vendor_id, :product_id, :path, :serial_number, :manufacturer_string, :product_string, :usage, :interface_number
